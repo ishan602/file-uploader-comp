65aa5decbc29968dd2ce7227dad82a84eb497e4f
ishan <kumarishan602@gmail.com>
2020-04-11 15:23:38 +0530

    Added Test Case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from "./App"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("My App component", () => {
+  it("should show text for submit button", () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find("button");
+    expect(text.text().length).toBe(6);
+  });
+  it("should click the button and upload image should run", () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find("button");
+    button.simulate("click");
+    expect(true).toBe(true);
+  });
+  it("it should trigger onChange in input field", () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find("input");
+    button.simulate("click");
+    expect(true).toBe(true);
+  })
+})
